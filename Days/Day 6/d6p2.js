39a0dd42b47aec449cbdabe8aa27ce0e2648e353
FREDERICK <github@FREDERICK.win>
2020-12-06 08:13:38 +0100

    Add day 6 part 2

---
+++ Days/Day 6/d6p2.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const lines = fs.readFileSync('input', { encoding: 'utf-8' }).split('\n\r');
+
+let result = 0;
+
+lines.forEach(line => {
+    line = line.replace(/\n/g, '').trim();
+    const groups = line.split('\r');
+    const yesCount = new Set();
+    const wholeGroup = groups.join('');
+
+    [...wholeGroup].forEach(char => {
+        const charCount = wholeGroup.split(char).length-1;
+        if (charCount === groups.length) yesCount.add(char);
+    });
+
+    result += yesCount.size;
+});
+
+console.log('the answer is: ' + result);
